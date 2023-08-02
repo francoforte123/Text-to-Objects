251519bfdcfd4bff8d3261fd6f50bfb474efb040
Emmanuele La Duca <laducaemanuele20@gmail.com>
2023-08-02 11:00:48 +0200

    Aggiungere i file di progetto.

---
+++ Text to Objects/Answer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Text_to_Objects
+{
+    public class Answer
+    {
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+        public string IsAccepted { get; set; }
+    }
+}
---
+++ Text to Objects/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Text.Json;
+using Text_to_Objects;
+
+string urlRequest = "https://localhost:7233/Request";
+string urlResponse = "https://localhost:7233/Response";
+
+var deserializerOptions = new JsonSerializerOptions
+{
+    PropertyNameCaseInsensitive = true,
+};
+
+var serializerOptions = new JsonSerializerOptions
+{
+    WriteIndented = true,
+};
+
+//inerente all'url request
+var htttpClientRequest= new HttpClient();
+var uriRequest= new Uri(urlRequest);
+var resultRequest= await htttpClientRequest.GetAsync(uriRequest);
+
+Console.WriteLine("sto stampando in formato Json le domande");
+string readValueRequestToString= await resultRequest.Content.ReadAsStringAsync();
+var questions= JsonSerializer.Deserialize<List<Question>>(readValueRequestToString, deserializerOptions);
+
+string requestToJson= JsonSerializer.Serialize(questions, serializerOptions);
+
+Console.WriteLine(requestToJson);
+
+Console.WriteLine("------------------------------------------------------");
+
+//inerente all'url response
+var htttpClientResponse = new HttpClient();
+var uriResponse = new Uri(urlResponse);
+var resultResponse = await htttpClientResponse.GetAsync(uriResponse);
+
+Console.WriteLine("sto stampando in formato Json le risposte");
+string readValueResponseToString = await resultResponse.Content.ReadAsStringAsync();
+var answer = JsonSerializer.Deserialize<List<Question>>(readValueResponseToString, deserializerOptions);
+
+string responseToJson = JsonSerializer.Serialize(answer, serializerOptions);
+
+Console.WriteLine(responseToJson);
+
+
+
+
+
